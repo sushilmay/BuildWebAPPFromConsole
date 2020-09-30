46b34b93ad7b6539ae5faf15cc6651a98bcde45c
sushil <sushil.may@gmail.com>
2020-09-30 05:58:28 -0700

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BuildWebAPPFromConsole
+{
+    public class Startup
+    {
+
+        public void ConfigurationServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app,IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints=> {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Running Web Server");
+                });
+            });
+        }
+    }
+}
