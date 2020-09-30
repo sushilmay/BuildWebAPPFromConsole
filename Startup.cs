dfc2a3d92899a978a8291acf358683087bc0a60d
sushil <sushil.may@gmail.com>
2020-09-30 07:39:21 -0700

    add mvc to application

--- Startup.cs
+++ Startup.cs
@@ -14,16 +14,20 @@ namespace BuildWebAPPFromConsole
     public class Startup
     {
 
-        public void ConfigurationServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
+            //this method is used to add mvc in 3.0.1 version there are some more methods see commented below
+            //services.AddMvc();
+            
+            //like for web api may use this one
+            //services.AddControllers();  
 
+            services.AddControllersWithViews();
         }
 
         /// <summary>
         /// Configure all middelware(is http pipeline) here via use(),next() and map() method.
         /// </summary>
-        /// <param name="app"></param>
-        /// <param name="env"></param>
         public void Configure(IApplicationBuilder app,IWebHostEnvironment env)
         {
             //here env variable read it form launchSetting.json file and as we know launchsetting only used for development
@@ -36,10 +40,13 @@ namespace BuildWebAPPFromConsole
             app.UseEndpoints(endpoints=> {
 
                 //Diff  Map Vs MapGet is MapGet only use get request where Map may use get put and post etc.
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Running Web Server"+ env.EnvironmentName);
-                });
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Running Web Server"+ env.EnvironmentName);
+                //});
+                
+                endpoints.MapDefaultControllerRoute();
+
             });
         }
     }
