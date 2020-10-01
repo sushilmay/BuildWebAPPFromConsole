a95257419a09f8cd164fdfd6525f0ff8c6438951
sushil <sushil.may@gmail.com>
2020-10-01 02:47:01 -0700

    #41) Create form in asp.net core

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -35,5 +35,15 @@ namespace BuildWebAPPFromConsole.Controllers
         {
             return _bookRepository.SearchBook(bookName, authorName);
         }
+        public ViewResult AddNewBook()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult AddNewBook(BookModel bookModel)
+        {
+            return View();
+        }
     }
 }
