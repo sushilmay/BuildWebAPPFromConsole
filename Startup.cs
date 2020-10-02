8742b7067c42356958733c84c3d6858a25464a27
sushil <sushil.may@gmail.com>
2020-10-02 08:49:35 -0700

    #71) Partial view in asp.net core

--- Startup.cs
+++ Startup.cs
@@ -21,7 +21,7 @@ namespace BuildWebAPPFromConsole
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<BookStoreContext>(options=> options.UseSqlServer("Server=tcp:dev007.database.windows.net,1433;Initial Catalog=dev007;Persist Security Info=False;User ID=dev007;Password=;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
+            services.AddDbContext<BookStoreContext>(options=> options.UseSqlServer("Server=tcp:dev007.database.windows.net,1433;Initial Catalog=dev007;Persist Security Info=False;User ID=dev007;Password=S;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
             
             //AddMVC() vs AddControllersWithViews() vs AddControllers() vs AddRazorPages()
 
