71c2ef53a56d1e165be0b2a534f8e08d552feef5
sushil <sushil.may@gmail.com>
2020-10-01 22:33:06 -0700

    #58) Create group in asp.net core dropdown | SelectListGroup

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -39,15 +39,21 @@ namespace BuildWebAPPFromConsole.Controllers
         public ViewResult AddNewBook(bool isSuccess = false, int bookId = 0)
         {
             var model = new BookModel() { 
-               // Language = "2"
+                Language = "2"
             };
- 
+
+            var group1 = new SelectListGroup() { Name = "Group 1" };
+            var group2 = new SelectListGroup() { Name = "Group 2", Disabled = true };
+            var group3 = new SelectListGroup() { Name = "Group 3" };
+
             ViewBag.Language = new List<SelectListItem>()
             {
-                new SelectListItem(){Text = "Hindi", Value = "1" },
-                new SelectListItem(){Text = "English", Value = "2", Disabled = true },
-                new SelectListItem(){Text = "Dutch", Value = "3", Selected = true },
-                new SelectListItem(){Text = "Tamil", Value = "4", Disabled = true },
+                new SelectListItem(){Text = "Hindi", Value = "1", Group = group1 },
+                new SelectListItem(){Text = "English", Value = "2", Group = group1 },
+                new SelectListItem(){Text = "Dutch", Value = "3", Group = group2},
+                new SelectListItem(){Text = "Tamil", Value = "4", Group = group2 },
+                new SelectListItem(){Text = "Urdu", Value = "5" , Group = group3},
+                new SelectListItem(){Text = "Chinese", Value = "6", Group = group3 },
             };
             ViewBag.IsSuccess = isSuccess;
             ViewBag.BookId = bookId;
@@ -58,12 +64,18 @@ namespace BuildWebAPPFromConsole.Controllers
         [HttpPost]
         public async Task<IActionResult> AddNewBook(BookModel bookModel)
         {
+            var group1 = new SelectListGroup() { Name = "Group 1" };
+            var group2 = new SelectListGroup() { Name = "Group 2", Disabled = true };
+            var group3 = new SelectListGroup() { Name = "Group 3" };
+
             ViewBag.Language = new List<SelectListItem>()
             {
-                new SelectListItem(){Text = "Hindi", Value = "1" },
-                new SelectListItem(){Text = "English", Value = "2", Disabled = true },
-                new SelectListItem(){Text = "Dutch", Value = "3", Selected = true },
-                new SelectListItem(){Text = "Tamil", Value = "4", Disabled = true },
+                new SelectListItem(){Text = "Hindi", Value = "1", Group = group1 },
+                new SelectListItem(){Text = "English", Value = "2", Group = group1 },
+                new SelectListItem(){Text = "Dutch", Value = "3", Group = group2},
+                new SelectListItem(){Text = "Tamil", Value = "4", Group = group2 },
+                new SelectListItem(){Text = "Urdu", Value = "5" , Group = group3},
+                new SelectListItem(){Text = "Chinese", Value = "6", Group = group3 },
             };
 
             if (ModelState.IsValid)
