6730bdcf64c47d1f489814dc9103ca0f5dba91f4
sushil <sushil.may@gmail.com>
2020-10-02 23:45:20 -0700

    #83) Read configuration using GetValue method from appsettings.json file

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace BuildWebAPPFromConsole.Controllers
 {
     public class HomeController : Controller// For MVC inherit by Controller for Web API Controller hinherit by BaseController class
     {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public ViewResult Index()
         {
+            bool result = _configuration.GetValue<bool>("NewBookAlert:DisplayNewBookAlert");
+            string bookName = _configuration.GetValue<string>("NewBookAlert:BookName");
+
             return View();
         }
         public ViewResult Aboutus()
