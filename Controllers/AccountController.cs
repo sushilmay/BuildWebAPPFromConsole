a9b62dcabc5f2f517d442edf85ca5a8bcf9b1c90
sushil <sushil.may@gmail.com>
2020-10-03 01:21:51 -0700

    #92) Design a registration (Signup) page in asp.net core

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BuildWebAPPFromConsole.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildWebAPPFromConsole.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("signup")]
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [Route("signup")]
+        [HttpPost]
+        public IActionResult Signup(SignUpUserModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // write your code
+
+                ModelState.Clear();
+            }
+            return View();
+        }
+    }
+}
